97339ec5397f7befba49f0883bca1d19154fdb5f
SagiBeno <benosagi321@gmail.com>
2025-11-09 16:30:07 +0100

    Start style

--- dusza/src/main/java/main/dusza/render/GameController.java
+++ dusza/src/main/java/main/dusza/render/GameController.java
@@ -9,6 +9,7 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.HBox;
@@ -32,9 +33,12 @@ public class GameController implements Initializable {
     @FXML public MFXButton nextButton;
     @FXML public Label task;
     @FXML public AnchorPane collectionContainer;
+    @FXML public ScrollPane collectionScroll;
+    @FXML public AnchorPane collectionPane;
 
     public static Player currentPlayer = GameData.PlayersList.getFirst();
     public static ArrayList<Card> playerDeck = new ArrayList<>();
+
     ArrayList<Card> playerCollection = new ArrayList<>();
 
     int collectionHalfSize = 0;
